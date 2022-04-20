836e6c551225b4e802f6b55e21fc3be73fd77a06
Fernando López Ramírez <flopezramirez@hotmail.com>
2022-04-20 12:56:46 -0500

    Server Script

---
+++ server.py
@@ -0,0 +1,27 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+# import json
+
+hostName = "localhost"
+serverPort = 8081
+
+
+class LocobotServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # file = self.path.replace('/', '')
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write({"test": "ok"})
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), LocobotServer)
+    print("Server started at http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
