75a0ecd7c83b792b749063b68f35b572cfd7a03d
Fernando López Ramírez <flopezramirez@hotmail.com>
2022-05-10 12:57:39 -0500

    Test Continuous Sending

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
+'''
 import socket
 
 PORT = 5050  # socket.gethostbyname(socket.gethostname())
-NUM_REQUESTS = 5
+NUM_REQUESTS = 10
 BUFFER = 1024
+ADDRESS = socket.gethostbyname(socket.gethostname())
+
+print(ADDRESS)
 
 my_socket = socket.socket()
-my_socket.bind(('10.50.115.95', PORT))
+my_socket.bind((ADDRESS, PORT))
 my_socket.listen(NUM_REQUESTS)
 
 while(True):
@@ -17,3 +21,33 @@ while(True):
     print(req)
     conn.send(bytes('Hello world!', encoding="utf8"))
     conn.close()
+
+'''
+import socket
+import time
+
+
+HEADERSIZE = 10
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1243))
+s.listen(5)
+
+while True:
+    # now our endpoint knows about the OTHER endpoint.
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established.")
+
+    msg = "Welcome to the server!"
+    msg = f"{len(msg):<{HEADERSIZE}}"+msg
+
+    clientsocket.send(bytes(msg, "utf-8"))
+
+    while True:
+        time.sleep(1)
+        msg = f"The time is {time.time()}"
+        msg = f"{len(msg):<{HEADERSIZE}}"+msg
+
+        print(msg)
+
+        clientsocket.send(bytes(msg, "utf-8"))
